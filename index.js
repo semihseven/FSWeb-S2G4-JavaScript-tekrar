9ba0a0509e086bcf0800e90dd646aaa4d799b706
semih-7 <semihseven0@gmail.com>
2025-04-21 00:26:14 +0300

    güncelleme

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function CemberinCevresi(yaricap) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(yaricap,pi) {
-  return pi * Math.pow(yaricap,2);
+function CemberinAlani(yaricap,p) {
+  return p * Math.pow(yaricap,2);
 }
 //console.log(CemberinAlani(15,pi))
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
